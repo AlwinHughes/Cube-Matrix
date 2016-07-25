55d6d4230671dea724c6c1e13e244e0925a32347
Alwin Hughes <alwin.hughes@btopenworld.com>
2016-07-25 22:14:26 +0100

    matricies can be created and operation done to them

--- matrix.js
+++ matrix.js
@@ -2,15 +2,21 @@ var matAwidth;
 var matAheight;
 var matBwidth;
 var matBheight;
+var matANSwidth;
+var matANSheight;
 
 var matA;
 var matB;
+var matANS;
 
 var cur_num_A_row = 0;
 var cur_num_A_col = 0;
 
 var cur_num_B_row = 0;
 var cur_num_B_col = 0;
+
+var cur_num_ANS_row = 0;
+var cur_num_ANS_col = 0;
 
 function createMatrix(){
 	matAwidth = parseInt(document.getElementById('matAwidth').value);
@@ -131,9 +137,69 @@ function matAupdate(element){
 	console.log(matA);
 }
 
-function matBupdate (element) {
+function matBupdate(element){
 	var pos = Array(2);
 	pos[1] = element.parentElement.id.split("_")[2];
 	pos[0] = element.parentElement.parentElement.id.split("_")[2];
 	matB[pos[0]][pos[1]] = parseFloat(element.value);
+}
+
+function calculate(){
+	var scope={
+		A: math.matrix(matA),
+		B: math.matrix(matB),
+		D: 1,
+		C: 1
+	}
+	var input = document.getElementById('input').value;
+	console.log("input: "+input);
+	console.log("includes A^-"+ input.includes("A^-"));
+	if(input.includes("B^-")||input.includes("B^(-")){
+		scope.D = math.inv(scope.B);
+		input = input.replace("B^-","D^")
+	}
+	if(input.includes("A^-")||input.includes("A^(-")){
+		scope.C = math.inv(scope.A)
+		input = input.replace("A^-","C^")
+	}
+	matANS = math.eval(input,scope);
+	matANSheight = matANS.size()[0];
+	matANSwidth = matANS.size()[1];
+
+	if(cur_num_ANS_row<matANSheight){
+			for(var i = cur_num_ANS_row; i<matANSheight;i++){
+				$('#matANS').append("<tr id='ANS_row_"+i+"'></tr>");	
+			}	
+		}else if(cur_num_ANS_row>matANSheight){
+			for(var i = cur_num_ANS_row-1; i>matANSheight-1;i--){
+				$('#ANS_row_'+i).remove();
+			}
+		}
+		cur_num_ANS_row = matANSheight;
+		if(cur_num_ANS_col<matANSwidth){
+			for(var i = 0; i<matANSheight;i++){
+				if($('#ANS_row_'+i +' > td').length<matANSwidth+1){
+					var num_of_rows = $('#ANS_row_'+i +' > td').length;
+					for(var j = num_of_rows; j<matANSwidth;j++){
+						$('#ANS_row_'+i).append("<td id='ANS_col_"+j+"'>"+matANS._data[i][j]+"</td>");
+					}
+				}
+			}
+		}else if(cur_num_ANS_col+1>matANSwidth){
+			console.log("remove colom")
+			for(var i = 0; i<matANSheight;i++){
+				if($('#ANS_row_'+i).children().length>matANSwidth){
+					for(var j = $('#ANS_row_'+i+' > td').length-1; j>matANSwidth-1;j--){
+						$('#ANS_col_'+j).remove();
+					}
+				}
+			}
+		}
+		for(var i = 0; i<matANSheight;i++){
+			for(var j = 0; j<matANSwidth;j++){
+				$("#ANS_row_"+i+" #ANS_col_"+j).text(matANS._data[i][j]);
+			}
+		}
+		cur_num_ANS_col = matANSwidth-1;
+
 }
