56ddbeb865249a8f51da9917938ee6346a604787
Alwin Hughes <alwin.hughes@btopenworld.com>
2016-08-04 12:22:35 +0100

    when in cube mode matricies can be rotated

--- cube.js
+++ cube.js
@@ -8,12 +8,38 @@ function cubeOptionsClick(){
 			matB = IDENTITY9;
 			resizeMatA(9,9,true,true);
 			resizeMatB(9,9,true,true);
+			$('#matAcont').append('<center> <button onclick="matARotateAClock()">Rotate Anticlockwise </button> <button onclick="matARotateClock()">Rotate Clockwise </button> </center>')
+			$('#matBcont').append('<center> <button onclick="matBRotateAClock()">Rotate Anticlockwise </button> <button onclick="matBRotateClock()">Rotate Clockwise </button> </center>')
 		}else{
 			matA = IDENTITY3;
 			matB = IDENTITY3;
 			resizeMatA(3,3,true,true);
 			resizeMatB(3,3,true,true);
+			$('#matAcont > center').remove();
+			$('#matBcont > center').remove();
 		}
 
 	}
 }
+
+function matARotateAClock() {
+	matA = math.multiply(math.matrix(PLL_SCOPE.Up),matA)._data;
+	resizeMatA(matA.length,matA[0].length,true);
+}
+
+function matARotateClock(){
+	matA = math.multiply(math.matrix(PLL_SCOPE.U),matA)._data;
+	resizeMatA(matA.length,matA[0].length,true);
+}
+
+
+function matBRotateAClock() {
+	matB = math.multiply(math.matrix(PLL_SCOPE.Up),matB)._data;
+	resizeMatB(matA.length,matB[0].length,true);
+}
+
+function matBRotateClock(){
+	matB = math.multiply(math.matrix(PLL_SCOPE.U),matB)._data;
+	resizeMatB(matA.length,matB[0].length,true);
+}
+
